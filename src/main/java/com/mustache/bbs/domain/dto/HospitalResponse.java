3cb621ca6bdc79a747c6615b3e4afd8bfdbb056b
Koogling <rnrudejr1234@naver.com>
2022-11-15 11:55:28 +0900

    HospitalRestController 추가, api로 json형식 데이터 확인

---
+++ src/main/java/com/mustache/bbs/domain/dto/HospitalResponse.java
@@ -0,0 +1,30 @@
+package com.mustache.bbs.domain.dto;
+
+import com.mustache.bbs.domain.entity.Hospital;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class HospitalResponse {
+    private Integer id;
+    private String roadNameAddress;
+    private String hospitalName;
+    private Integer patientRoomCount;
+    private Integer totalNumberOfBeds;
+    private String businessTypeName;
+    private Float totalAreaSize;
+
+    public HospitalResponse(Integer id, String roadNameAddress, String hospitalName, Integer patientRoomCount, Integer totalNumberOfBeds, String businessTypeName, Float totalAreaSize) {
+        this.id = id;
+        this.roadNameAddress = roadNameAddress;
+        this.hospitalName = hospitalName;
+        this.patientRoomCount = patientRoomCount;
+        this.totalNumberOfBeds = totalNumberOfBeds;
+        this.businessTypeName = businessTypeName;
+        this.totalAreaSize = totalAreaSize;
+    }
+
+}
