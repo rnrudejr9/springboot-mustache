e6fab51e1ff191b5a4e9ffd019d12168ca677d24
pi koo <rnrudejr1234@naver.com>
2022-12-03 14:03:02 +0900

    dto 생성자 수정

--- src/main/java/com/mustache/bbs/domain/dto/article/ArticleDto.java
+++ src/main/java/com/mustache/bbs/domain/dto/article/ArticleDto.java
@@ -16,12 +16,6 @@ public class ArticleDto {
     private String content;
     private String title;
 
-    public ArticleDto(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
     @Override
     public String toString(){
         return this.content + " " + this.id + " " + this.title;
